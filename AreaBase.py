b20b1928fabf9ee69862c6229c8a7e826cffda26
Victor Sevilla <victorslim2006@gmail.com>
2025-09-25 12:14:48 +0200

    Ejercicio Area Base

---
+++ AreaBase.py
@@ -0,0 +1,8 @@
+altura = float(input("Dime la altura en metros: "))
+base = float(input("Dime la base en metros: "))
+
+area = altura * base
+perimetro = 2 * (altura + base)
+
+print("El área del rectángulo es:", area)
+print("El perímetro del rectángulo es:", perimetro)
