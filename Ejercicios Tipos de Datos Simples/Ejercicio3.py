11ca3f3a4006a8d1763ccc2fd20c7bc9c3ce6194
Victor  Sevilla <victorslim2006@gmail.com>
2025-10-06 12:34:58 +0200

    Delete Ejercicios Tipos de Datos Simples directory

--- Ejercicios Tipos de Datos Simples/Ejercicio3.py
+++
@@ -1,2 +0,0 @@
-nombre = input("¿Cómo te llamas?")
-print("¡Hola " + nombre + "!")
--- Ejercicios Tipos de Datos Simples/Ejercicio7.py
+++
@@ -1,4 +0,0 @@
-peso = float(input("Introduce tu peso en kg: "))
-altura = float(input("Introduce tu altura en metros: "))
-imc = peso / (altura ** 2)
-print("Tu indice de mas corporal es: {:.2f}".format(imc))
