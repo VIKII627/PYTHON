9dc43fb259bd90314360b5aa5bdbae98fe77cea7
Victor Sevilla <victorslim2006@gmail.com>
2025-10-09 09:37:30 +0200

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,7 @@
+num_payasos = int(input("Ingrese el número de payasos vendidos: "))
+num_munecas = int(input("Ingrese el número de muñecas vendidas: "))
+peso_payaso = 112
+peso_muneca = 75
+peso_total = (num_payasos * peso_payaso) + (num_munecas * peso_muneca) 
+
+print(f"El peso total del paquete es: {peso_total} gramos")
