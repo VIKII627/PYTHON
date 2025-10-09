27f30f67361001871b70424bb3f8732960e74fc1
Victor Sevilla <victorslim2006@gmail.com>
2025-10-09 10:26:19 +0200

    EjercicioCadenas9

---
+++ EjercicioCadenas9.py
@@ -0,0 +1,7 @@
+fecha = input("Introduce tu fecha de nacimiento (dd/mm/aaaa): ")
+dia = fecha.split("/")[0]
+mes = fecha.split("/")[1]
+anio = fecha.split("/")[2]
+print("Día: ", dia)
+print("Mes: ", mes)
+print("Año: ", anio)
