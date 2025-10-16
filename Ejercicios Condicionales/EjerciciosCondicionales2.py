775ac7552208d42fa3f7d7826aa0349021c6d41f
Victor Sevilla <victorslim2006@gmail.com>
2025-10-16 09:49:38 +0200

    EjerciciosCondicionales2

---
+++ Ejercicios Condicionales/EjerciciosCondicionales2.py
@@ -0,0 +1,7 @@
+contraseña = "HolaMundo123"
+entrada = input("Por favor, ingrese la contraseña: ")   
+
+if entrada.lower() == contraseña.lower():
+    print ("Contraseña correcta.")
+else:
+    print ("Contraseña incorrecta.")   
