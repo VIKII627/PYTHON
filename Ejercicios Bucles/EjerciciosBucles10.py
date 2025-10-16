50b98b50c6c77d9fc1d203bbcdcc90a9ab661825
Victor Sevilla <victorslim2006@gmail.com>
2025-10-16 11:14:21 +0200

    EjerciciosBucles10

---
+++ Ejercicios Bucles/EjerciciosBucles10.py
@@ -0,0 +1,17 @@
+#Haz un programa que pida aal usuario un numero entero positivo y 
+#con bucles que pruebe con todos los numeros desde 1 hasta el suyo mismo
+#y si dicho numero es divisible solo por si mismo, es primo sino, no lo es.
+#hacer con bucle while
+
+numero = int(input("Introduce un número entero positivo: "))
+i = 1
+es_primo = True
+while i <= numero:
+    if numero % i == 0 and i != 1 and i != numero:
+        es_primo = False
+        break
+    i += 1
+if es_primo:
+    print(f"El número {numero} es primo.")
+else:
+    print(f"El número {numero} no es primo.")
