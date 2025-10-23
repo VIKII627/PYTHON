294fd146818cab461930ad85ddb61637c78be4ac
Victor Sevilla <victorslim2006@gmail.com>
2025-10-23 09:38:42 +0200

    Modificacion EjerciciosBucles13

--- Ejercicios Bucles/EjerciciosBucles13.py
+++ Ejercicios Bucles/EjerciciosBucles13.py
@@ -1,6 +1,3 @@
-#Ejercicio 13
-#Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta
-#que el usuario escriba “salir” que terminará.  
 while True:
     texto = input("Introduce un texto (escribe 'salir' para terminar): ")
     if texto.lower() == "salir":
