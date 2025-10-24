ec1949d9d6f3849b3b24c35aaa3dc6e8ad367b34
Victor Sevilla <victorslim2006@gmail.com>
2025-10-24 14:27:05 +0200

    TuplaLista1

--- Ejercicios Tuplas y Listas/TuplaLista1.py
+++ Ejercicios Tuplas y Listas/TuplaLista1.py
@@ -2,3 +2,8 @@
 #Escribir un programa que almacene las asignaturas de un curso (por ejemplo
 #Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por
 #pantalla.
+
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+print("Las asignaturas del curso son:")
+for asignatura in asignaturas:
+    print(asignatura)
