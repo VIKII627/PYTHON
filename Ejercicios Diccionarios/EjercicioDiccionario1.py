4649fc5c33df50d37d5943ca3f086893240ec58b
Victor Sevilla <victorslim2006@gmail.com>
2026-01-08 11:09:03 +0100

    Modificacion del EjercicioDiccionario1 para mas compacto y limpio

--- Ejercicios Diccionarios/EjercicioDiccionario1.py
+++ Ejercicios Diccionarios/EjercicioDiccionario1.py
@@ -1,7 +1,7 @@
 #Ejercicio 1 Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario 
 #por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
 Diccionario = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
-divisa = input("¿Cual es su divisa? (Escriba un simbolo o un mensaje): ")
+divisa = input("¿Cual es su divisa?")
 
 if divisa in Diccionario:
     print("El simbolo de la divisa es: ", Diccionario[divisa])
