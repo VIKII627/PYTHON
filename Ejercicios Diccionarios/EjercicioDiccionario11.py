76f1dddd1ddfae5efed1f0a478cf450471ebf1d9
Victor Sevilla <victorslim2006@gmail.com>
2026-01-15 10:49:07 +0100

    Intento de la solucion del ejercicio11 de diccionarios

--- Ejercicios Diccionarios/EjercicioDiccionario11.py
+++ Ejercicios Diccionarios/EjercicioDiccionario11.py
@@ -13,3 +13,9 @@
 # correspondientes a los campos. 
 # Es decir, un diccionario como el siguiente 
 # {'01234567L': {'nombre': 'Luis González', 'email': 'luisgonzalez@mail.com', 'teléfono': '656343576', 'descuento': 12.5}, '71476342J': {'nombre': 'Macarena Ramírez', 'email': 'macarena@mail.com', 'teléfono': '692839321', 'descuento': 8.0}, '63823376M': {'nombre': 'Juan José Martínez', 'email': 'juanjo@mail.com', 'teléfono': '664888233', 'descuento': 5.2}, '98376547F': {'nombre': 'Carmen Sánchez', 'email': 'carmen@mail.com', 'teléfono': '667677855', 'descuento': 15.7}} 
+
+datos = "nif;nombre;email;teléfono;descuento\n"
+lineas = datos.split("\n")
+
+directorio = {}
+
